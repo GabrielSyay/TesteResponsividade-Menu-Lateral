4ef9d266661910f99da4418d846ad0951d335e9d
Gabriel <gabrielvasco350@gmail.com>
2024-10-22 22:03:08 -0300

    Finalização-Do-Dia

--- app/script.js
+++ app/script.js
@@ -1,2 +1,11 @@
-
-
+$(document).ready(() => {
+    $(".hamburguer").click(function () {
+      if (!$(this).hasClass("active")) {
+        $(this).addClass("active");
+        $(".menu").addClass("active");
+      } else {
+        $(this).removeClass("active");
+        $(".menu").removeClass("active");
+      }
+    });
+  })
